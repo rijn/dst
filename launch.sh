4149d291203507d6e484b317832cde2ad1804d1a
Rijn <bxbian951122@gmail.com>
2017-06-27 22:49:26 -0500

    remove validate option

--- launch.sh
+++ launch.sh
@@ -5,6 +5,7 @@ install_dir="$HOME/dontstarvetogether_dedicated_server"
 cluster_name="Cluster_1"
 dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
 
+check_for_file "$install_dir/mods"
 cp -f dedicated_server_mods_setup.lua "$install_dir/mods/dedicated_server_mods_setup.lua"
 mkdir "$dontstarve_dir/$cluster_name"
 cp -f cluster.ini "$dontstarve_dir/$cluster_name/cluster.ini"
@@ -38,7 +39,7 @@ check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
 check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
-./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 +quit
 
 check_for_file "$install_dir/bin"
 
